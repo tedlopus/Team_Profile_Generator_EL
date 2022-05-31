4278a914c9d1c02af44c3ff096fec454bcd2a859
tedlopus <tedlopus@gmail.com>
2022-05-30 22:19:22 -0600

    Added test files and started on index.js

--- index.js
+++ index.js
@@ -1,3 +1,63 @@
 const fs = require('fs'); 
 const inquirer = require('inquirer');
 
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const { exit } = require('process');
+
+team = [];
+
+const runGenerator = () => {
+
+    const addMember = () => {
+        return inquirer.prompt ([
+            {
+                type: "list",
+                name: "member",
+                message: "Please add a team member, otherwise select exit.",
+                choices:[
+                    "Add Engineer",
+                    "Add Intern",
+                    "Exit",
+                ],
+            }.then( answer => {
+                switch (answer.member) {
+                    case "Engineer":
+                        addEngineer();
+                        break;
+
+                    case "Intern":
+                        addIntern();
+                        break;
+                    
+                    default:
+                        exit();
+                }
+            })
+        ])
+    };
+
+    const addEngineer = () => {
+        return inquirer.prompt ([
+            {
+                type: "list",
+                name: "member",
+                message: "Please add a team member, otherwise select exit.",
+                choices:[
+                    "Add Engineer",
+                    "Add Intern",
+                    "Exit",
+                ],
+            }
+
+
+        ])
+    };
+
+
+
+
+
+
+};
